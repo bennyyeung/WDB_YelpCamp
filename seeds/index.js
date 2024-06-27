070e3dc8eb0c77eaff4ebb4df233fc7baf6049d7
Benny Yeung <benny.tf@gmail.com>
2024-06-27 17:15:06 +0100

    YelpCamp:Basic Authorization - Adding an Author to Campground

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '667d89ad4f77d157d330965f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://unsplash.it/1920/1080?random/',
